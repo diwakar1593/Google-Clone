eef54f4327bc3d8735838654ba0bff8492f946c8
diwakar1593 <diwakar200kumar@gmail.com>
2023-03-26 02:13:03 +0530

    update

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__header">
+                <div className="home__headerLeft">
+                    <p>About</p>
+                    <p>Store</p>
+                </div>
+
+                <div className="home__headerRight">
+                    <p>Gmail</p>
+                    <p>Images</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
