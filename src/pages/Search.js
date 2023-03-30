2d28e21076379f91ceb1c2db72766753375e928c
diwakar1593 <diwakar200kumar@gmail.com>
2023-03-30 19:31:36 +0530

    commit

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,21 @@
 import React from "react"
 import "./Search.css";
-
+import SearchIcon from '@mui/icons-material/Search';
+import MicIcon from '@mui/icons-material/Mic';
+import { Button } from "@mui/material";
 function Search() {
     return (
-        <div>
-            Search Compontents.
-        </div>
+        <form className="search">
+            <div className="search__input">
+                <SearchIcon className="searchicon"/>
+                <input/>
+                <MicIcon/>
+            </div>
+            <div className="search__buttons">
+                <Button type="submit" variant="outlined">Google Search</Button>
+                <Button type="submit" variant="outlined">I'm Feeling Lucky</Button>
+            </div>
+        </form>
     )
 }
 
