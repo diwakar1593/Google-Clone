c1b37b5a37bde8aadce6fd1a1c0b333fc4e53530
diwakar1593 <diwakar200kumar@gmail.com>
2023-04-03 02:55:56 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import Home from './pages/Home';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import SearchPage from './pages/SearchPage';
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Home/>
+    <Switch>
+          <Route exact path="/search">
+            <SearchPage />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
     </div>
+    </Router>
   );
 }
 
